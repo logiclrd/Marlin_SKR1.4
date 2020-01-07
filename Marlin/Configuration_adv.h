704fb2da433b306e0716fc7dd03d136656bb1631
Jonathan Gilbert <JonathanG@iQmetrix.com>
2020-01-06 20:07:07 -0600

    Enabled FILAMENT_RUNOUT_SENSOR, NOZZLE_PARK_FEATURE and ADVANCED_PAUSE_FEATURE, including ADVANCED_PAUSE_FANS_FAUSE. Changed the filament runout sensor port from X+ to Z+ in pins_BTT_SKR_V1_3.h, since that is where I happen to have it plugged in. Changed DEFAULT_AXIS_STEPS_PER_UNIT to 8000 for Z.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1722,7 +1722,7 @@
  * Requires NOZZLE_PARK_FEATURE.
  * This feature is required for the default FILAMENT_RUNOUT_SCRIPT.
  */
-//#define ADVANCED_PAUSE_FEATURE
+#define ADVANCED_PAUSE_FEATURE
 #if ENABLED(ADVANCED_PAUSE_FEATURE)
   #define PAUSE_PARK_RETRACT_FEEDRATE         60  // (mm/s) Initial retract feedrate.
   #define PAUSE_PARK_RETRACT_LENGTH            2  // (mm) Initial retract.
@@ -1748,7 +1748,7 @@
                                                   //   Filament can be extruded repeatedly from the Filament Change menu
                                                   //   until extrusion is consistent, and to purge old filament.
   #define ADVANCED_PAUSE_RESUME_PRIME          0  // (mm) Extra distance to prime nozzle after returning from park.
-  //#define ADVANCED_PAUSE_FANS_PAUSE             // Turn off print-cooling fans while the machine is paused.
+  #define ADVANCED_PAUSE_FANS_PAUSE             // Turn off print-cooling fans while the machine is paused.
 
                                                   // Filament Unload does a Retract, Delay, and Purge first:
   #define FILAMENT_UNLOAD_PURGE_RETRACT       13  // (mm) Unload initial retract length.
--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_3.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_3.h
@@ -73,7 +73,7 @@
 // Filament Runout Sensor
 //
 #ifndef FIL_RUNOUT_PIN
-  #define FIL_RUNOUT_PIN   P1_28
+  #define FIL_RUNOUT_PIN   P1_24
 #endif
 
 //
