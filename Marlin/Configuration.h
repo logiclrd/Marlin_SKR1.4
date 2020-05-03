a48765bbd3ad0d5e812c3c1c8cd32d3d6b5f1dd0
Jonathan Gilbert <JonathanG@iQmetrix.com>
2020-05-03 16:26:48 -0500

    Changes to Configuration.h: - Adjusted NOZZLE_TO_PROBE_OFFSET following testing. - Set Z_HOMING_HEIGHT_20 to ensure clearance between the nozzle and bed clips. - Set Z_MAX_POS to 400. Interim fix to src/module/motion.cpp, updating CAN_HOME_Z to permit Z homing without a ZMIN pin defined.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -971,7 +971,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -41.5, 2.5, -1 }
+#define NOZZLE_TO_PROBE_OFFSET { -41.5, 2.5, -1.9 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1093,7 +1093,7 @@
 
 //#define UNKNOWN_Z_NO_RAISE      // Don't raise Z (lower the bed) if Z is "unknown." For beds that fall when Z is powered off.
 
-//#define Z_HOMING_HEIGHT  4      // (mm) Minimal Z height before homing (G28) for Z clearance above the bed, clamps, ...
+#define Z_HOMING_HEIGHT  20       // (mm) Minimal Z height before homing (G28) for Z clearance above the bed, clamps, ...
                                   // Be sure to have this much clearance over your Z_MAX_POS to prevent grinding.
 
 //#define Z_AFTER_HOMING  10      // (mm) Height to move to after homing Z
@@ -1116,7 +1116,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 200
+#define Z_MAX_POS 400
 
 /**
  * Software Endstops
