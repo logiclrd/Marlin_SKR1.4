216e95e77524daac7fdc40b85148570b4e34cc4c
Jonathan Gilbert <JonathanG@iQmetrix.com>
2020-08-18 23:47:58 -0500

    Adjusted X_MAX_POS and Y_MAX_POS based on actual measured possible coordinate ranges. Increased PROBING_MARGIN to 25 so that the rightmost column of samples will be within the reachable range according to X_MAX_POS and NOZZLE_TO_PROBE_OFFSET.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1009,7 +1009,7 @@
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 20
+#define PROBING_MARGIN 25
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
@@ -1148,8 +1148,8 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS -9
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
+#define X_MAX_POS 416
+#define Y_MAX_POS 392
 #define Z_MAX_POS 400
 
 /**
